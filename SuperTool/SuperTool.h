23b349d501a0ee2fa03bc67311796cf6eba17c2c
Simon Cozens <simon@simon-cozens.org>
2016-04-21 14:28:35 +1000

    Initial Commit

---
+++ SuperTool/SuperTool.h
@@ -0,0 +1,18 @@
+//
+//  SuperTool.h
+//  SuperTool
+//
+//  Created by Simon Cozens on 21/04/2016.
+//  Copyright © 2016 Simon Cozens. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import <GlyphsCore/GlyphsToolDrawProtocol.h>
+#import <GlyphsCore/GlyphsToolEventProtocol.h>
+#import <GlyphsCore/GlyphsPathPlugin.h>
+
+@interface SuperTool : GlyphsPathPlugin {
+
+}
+
+@end
